ac285a455c6d5ffac408a6f8a32b583761b3ce2b
Procellarum123 <boure1234@seznam.cz>
2024-10-28 17:14:08 +0100

    Initial Push

---
+++ Source/TestProjectWithGit/TestProjectWithGit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestProjectWithGit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestProjectWithGit, "TestProjectWithGit" );
